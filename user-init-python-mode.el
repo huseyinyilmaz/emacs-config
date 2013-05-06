75f9c90fdcf97b07d7283288d0e11cf36b517b7f
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2013-05-06 15:21:30 +0300

    Add subword-mode for python-mode

--- user-init-python-mode.el
+++ user-init-python-mode.el
@@ -2,6 +2,9 @@
 	  #'(lambda ()
 	      (setq py-shell-name "ipython")
 	      (require 'python-mode)))
+
+(add-hook 'python-mode-hook #'subword-mode)
+
 (message "config python-mode")
 (provide 'user-init-python-mode)
 ;;; user-init-python-mode.el ends here
