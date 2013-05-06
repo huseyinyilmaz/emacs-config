27b7a3e1d0116295b1e9b25688ca5c7fb04e5236
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2013-05-06 09:33:43 +0300

    changed changed autocomplete jedi backend with . activated jedi backend

--- user-init-jedi.el
+++ user-init-jedi.el
@@ -1,7 +1,9 @@
 (add-hook 'after-init-hook
 	  #'(lambda ()
 	      (setq jedi:setup-keys t)
-	      (add-hook 'python-mode-hook 'jedi:ac-setup)))
+	      ;; (add-hook 'python-mode-hook 'jedi:ac-setup)
+	      (add-hook 'python-mode-hook 'jedi:setup)
+	      (setq jedi:complete-on-dot t)))
 (message "config jedi")
 (provide 'user-init-jedi)
 ;;; user-init-jedi.el ends here
