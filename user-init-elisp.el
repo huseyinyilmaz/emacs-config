5d5f6f40882a752b75678f010b5edb276a3bb7af
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2013-12-03 13:28:51 +0200

    Fixed hook name bug

--- user-init-elisp.el
+++ user-init-elisp.el
@@ -6,14 +6,10 @@
 ;;; Code:
 
  ;; for elisp files
-(add-hook 'emacs-lisp-mode #'turn-on-eldoc-mode)
+(add-hook 'emacs-lisp-mode-hook #'turn-on-eldoc-mode)
+
 ;; for scratch buffer
-(add-hook 'lisp-interaction-mode #'turn-on-eldoc-mode)
-;; scratch is already started
-(add-hook 'after-init-hook
-	  #'(lambda ()
-	      (with-current-buffer (get-buffer "*scratch*")
-		(turn-on-eldoc-mode))))
+(add-hook 'lisp-interaction-mode-hook #'turn-on-eldoc-mode)
 
 (message "config emacs lisp mode")
 (provide 'user-init-elisp)
