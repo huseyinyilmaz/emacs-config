076c6f1f85ef2b015d4d99e146e9c78c6b905209
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2013-12-05 13:51:30 +0200

    Added git gutter.

--- user-init-config.el
+++ user-init-config.el
@@ -22,6 +22,7 @@
 			;; color-theme-solarized
 			solarized-theme
                         rainbow-delimiters
+                        git-gutter-fringe
 			))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;
---
+++ user-init-git.el
@@ -0,0 +1,16 @@
+;;; user-init-git.el --- Git configuration file.
+
+
+;;; Commentary:
+;; 
+
+;;; Code:
+
+(add-hook 'after-init-hook #'user-init-configure-git)
+
+(defun user-init-configure-git()
+  (global-git-gutter-mode))
+
+(provide 'user-init-git)
+
+;;; user-init-git.el ends here
