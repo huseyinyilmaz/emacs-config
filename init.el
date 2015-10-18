74a0436e2da9a7267354c30a6987c9f5d6273069
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2015-10-17 21:44:40 -0700

    removes safe-theme variable since the value changes on every install.

--- init.el
+++ init.el
@@ -7,16 +7,6 @@
 
 (require 'user-init)
 ;;; Code:
-
-(provide 'init)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -25,3 +15,7 @@
  '(flycheck-color-mode-line-error-face ((t (:inherit flycheck-fringe-error))))
  '(flycheck-color-mode-line-info-face ((t (:inherit flycheck-fringe-info))))
  '(flycheck-color-mode-line-warning-face ((t (:inherit flycheck-fringe-warning)))))
+
+(provide 'init)
+
+;;; init.el ends here
