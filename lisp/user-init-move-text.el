e8fb0d04214b06922954ff3bd741d698264f907a
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2016-08-31 16:50:30 -0700

    Remove extra log message

--- lisp/user-init-move-text.el
+++ lisp/user-init-move-text.el
@@ -5,8 +5,6 @@
 
 
 ;;; Code:
-(message "XXX--------XXX")
-
 (add-hook 'after-init-hook (lambda ()
                              (global-set-key (kbd "M-p") 'move-text-up)
                              (global-set-key (kbd "M-n") 'move-text-down)))
