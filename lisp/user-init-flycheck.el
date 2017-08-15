b34789ca9924caa7c39adf89898d611033ab39eb
huseyin <huseyin@grandfolk.com>
2017-08-15 09:40:18 -0700

    Update flycheck

--- lisp/user-init-flycheck.el
+++ lisp/user-init-flycheck.el
@@ -27,6 +27,21 @@
 (eval-after-load "flycheck"
   `(add-hook 'web-mode-hook (lambda () (tern-mode t))))
 
+;; use local eslint from node_modules before global
+;; http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
+
+
 
 
 (setq-default flycheck-flake8-maximum-complexity 10)
--- lisp/user-init-web-mode.el
+++ lisp/user-init-web-mode.el
@@ -53,6 +53,13 @@
               ))
 
 
+;; for better jsx syntax-highlighting in web-mode
+;; - courtesy of Patrick @halbtuerke
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+    (let ((web-mode-enable-part-face nil))
+      ad-do-it)
+    ad-do-it))
 
 
 (message "config web-mode")
